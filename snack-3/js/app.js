28462f6634932f249ae3eecfd3e1f1ee572486eb
brunEdo <edobruno.m@gmail.com>
2021-08-27 12:58:22 +0200

    snack 3 script

--- snack-3/js/app.js
+++ snack-3/js/app.js
@@ -1,5 +1,14 @@
 // 0. Create list of names and of surnames
+const names = ["Paolo", "Luigi", "Gabriele"];
+const surnames = ["Rossi", "Verdi", "Brambilla", "Magnani"];
 // 1. Create output array
-// 2. Get random name
-// 3. Get random surname
-// 4. Add joined strings in output array
+let outputList = [];
+for (let i = 0; i < 3; i++) {
+    // 2. Get random name
+    let randomName = names[Math.ceil(Math.random() * names.length - 1)];
+    // 3. Get random surname
+    let randomSurname = surnames[Math.ceil(Math.random() * surnames.length - 1)];
+    // 4. Add joined strings in output array
+    outputList.push(`${randomName} ${randomSurname}`);
+}
+console.log(outputList);
