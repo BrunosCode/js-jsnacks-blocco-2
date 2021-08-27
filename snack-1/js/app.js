faa61fd2aef3d2d32873eaf277f5f57d6dd6b0d4
brunEdo <edobruno.m@gmail.com>
2021-08-27 11:34:13 +0200

    js script

--- snack-1/js/app.js
+++ snack-1/js/app.js
@@ -1,6 +1,18 @@
 // 1. For loop: ask 5 times a number to a user
+let sum1 = 0;
+for (let i = 0; i < 5; i++) {
+    sum1 += parseInt(prompt("Give me a number"));
+}
 // 2. Print the sum
+console.log("first sum " + sum1)
 
 
 // 3. While loop
+let sum2 = 0;
+let counter = 0
+while(counter < 5) {
+    sum2 += parseInt(prompt("Give me a number"));
+    counter ++;
+}
 // 4. print the sum
+console.log("second sum " + sum2)
