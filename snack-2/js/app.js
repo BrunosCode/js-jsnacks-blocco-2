64945b6bc2cee229939005128fa420c13c1beb14
brunEdo <edobruno.m@gmail.com>
2021-08-27 12:18:18 +0200

    snack 2 script

--- snack-2/js/app.js
+++ snack-2/js/app.js
@@ -1,4 +1,14 @@
 // 1. ask a number
+let n = parseInt(prompt("Give me a number"));
 // 1.1 verify if is a number
+while ( isNaN(n) ) {
+    alert("That's not a number");
+    n = parseInt(prompt("Give me a number"));
+}
 // 2. if is even print it
+if ( n % 2 == 0) {
+    alert(n);
 // 3. if is odd add 1 and print
+} else {
+    alert(n + 1);
+}
