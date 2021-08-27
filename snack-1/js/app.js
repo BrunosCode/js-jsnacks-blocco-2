5ddd326ed648dbff466c747cadee8b7dc15d05b9
brunEdo <edobruno.m@gmail.com>
2021-08-27 11:50:44 +0200

    while to check number

--- snack-1/js/app.js
+++ snack-1/js/app.js
@@ -1,7 +1,12 @@
 // 1. For loop: ask 5 times a number to a user
 let sum1 = 0;
 for (let i = 0; i < 5; i++) {
-    sum1 += parseInt(prompt("Give me a number"));
+    let n = parseInt(prompt("Give me a number"));
+    while ( isNaN(n) ) {
+        alert("That's not a number");
+        n = parseInt(prompt("Give me a number"));
+    }
+    sum1 += n;
 }
 // 2. Print the sum
 console.log("first sum " + sum1)
@@ -11,7 +16,12 @@ console.log("first sum " + sum1)
 let sum2 = 0;
 let counter = 0
 while(counter < 5) {
-    sum2 += parseInt(prompt("Give me a number"));
+    let n = parseInt(prompt("Give me a number"));
+    while ( isNaN(n) ) {
+        alert("That's not a number");
+        n = parseInt(prompt("Give me a number"));
+    }
+    sum2 += n;
     counter ++;
 }
 // 4. print the sum
